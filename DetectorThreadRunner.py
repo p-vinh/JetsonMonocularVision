cc812c100236591b0ad9d0a86ee60eb5847922cd
UAV Jetson <vinhph003@gmail.com>
2024-10-25 12:40:51 -0700

    Changed detect model to default human

--- DetectorThreadRunner.py
+++ DetectorThreadRunner.py
@@ -20,7 +20,8 @@ class DetectorThreadRunner:
         self.spread = spread
         self.fov = fov
         self.GPS = GPS_node
-        self.d_net = jetson_inference.detectNet(model="model/best.onnx", labels="model/labels.txt", input_blob="input_0", output_blob="output_0")
+#        self.d_net = jetson_inference.detectNet(model="model/ssd-mobilenet.onnx", labels="model/labels.txt", input_blob="input_0", output_cvg="scores", output_bbox="boxes")
+        self.d_net = jetson_inference.detectNet()
         self.person_loc = None
         self.detection_time = None
         self.log_f = log_file
